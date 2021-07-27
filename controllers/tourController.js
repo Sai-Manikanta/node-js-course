a56fd10dde624f0106b759fe2f214ba4edc4a91c
mani333007 <mani333007@gmail.com>
2021-07-27 12:33:25 +0530

    intial commit

---
+++ controllers/tourController.js
@@ -0,0 +1,87 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/../dev-data/data/tours.json`)
+);
+
+exports.getTours = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        requestedAt: req.requestedTime,
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
+}
+
+exports.createTour = (req, res) => {
+    const newId = tours[tours.length - 1]._id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+}
+
+exports.getTour = (req, res) => {
+    const { id } = req.params;
+
+    const tour = tours.find(el => el._id === id);
+
+    if(!tour){
+        return res.status(404).json({
+            status: 'fail',
+            data: null
+        })
+    }
+    
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+}
+
+exports.updateTour = (req, res) => {
+    const { id } = req.params;
+
+    const tour = tours.find(el => el._id === id);
+
+    if(!tour){
+        return res.status(404).json({
+            status: 'fail',
+            data: 'Invalid ID' 
+        })
+    }
+    
+    res.status(200).json({
+        status: 'success',
+        message: '<Updated tour here...>'
+    })
+}
+
+exports.deleteTour =  (req, res) => {
+    const { id } = req.params;
+
+    const tour = tours.find(el => el._id === id);
+
+    if(!tour){
+        return res.status(404).json({
+            status: 'fail',
+            data: 'Invalid ID' 
+        })
+    }
+    
+    res.status(200).json({
+        status: 'success',
+        message: null
+    })
+}
---
+++ server.js
@@ -0,0 +1,4 @@
+const app = require('./app')
+// Starting server
+const port = 8000;
+app.listen(port, () => console.log(`App listening on port ${port}`))
